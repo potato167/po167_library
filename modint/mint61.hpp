e27b0ec0906af0682917003d934d11fda726a84c
potato167 <66621378+potato167@users.noreply.github.com>
2024-06-21 21:16:59 +0900

    modint とちゅう

---
+++ modint/mint61.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace po167{
+struct mint61{
+    using u64 = unsigned long long;
+    const u64 MOD = (1UL << 61) - 1;
+    const u64 MASK30 = (1UL << 30) - 1;
+    const u64 MASK31 = (1UL << 31) - 1;
+    u64  x = 0;
+    u64 calcmod(u64 a){
+        
+    }
+    mint61(long long x_){
+
+    }
+};
+}
